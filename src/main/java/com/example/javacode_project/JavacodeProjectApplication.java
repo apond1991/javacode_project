fb413082c8e38ca719fd468f413a659359f9a29b
apond1991 <debapon557@gmail.com>
2019-08-16 06:50:37 +0000

    javacode

---
+++ src/main/java/com/example/javacode_project/JavacodeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.javacode_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacodeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacodeProjectApplication.class, args);
+	}
+
+}
